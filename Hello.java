f1020c968bc1be5a203095cff0d5b13d4de46710
Hu-zhiqiang <1334666585@qq.com>
2019-10-28 16:20:36 +0800

    hello2

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World4");
+System.out.println("Hello World5");
+System.out.println("Hello World6");
+    }
+}
